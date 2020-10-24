988e28affcc9ce8a16dd8b36dea519d4e2c161dd
Pavel Slama <info@pavelslama.cz>
2020-10-24 20:26:14 +0200

    CI test

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,6 @@
+process.env.INFLUX_HOST = 'localhost';
+process.env.INFLUX_DB = 'db';
+process.env.INFLUX_PORT = '8087';
+process.env.INFLUX_USER = 'admin';
+process.env.INFLUX_PASSWORD = 'supersecretpassword';
+process.env.INFLUX_MEASUREMENT = 'test';
