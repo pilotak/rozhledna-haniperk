4980c70bd47affafac6f2e8aaead36cd2cb3de71
Pavel Slama <info@pavelslama.cz>
2022-01-10 17:01:56 +0100

    Revert "deps update"
    This reverts commit 1d5d80251e455426ae33d5a69169801cf142f012.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'public', 'scripts', 'index.ts'),
@@ -27,9 +27,6 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
   },
   resolve: {
     extensions: ['.ts', '.js', '.scss'],
@@ -63,7 +60,7 @@ module.exports = {
       },
     }),
     //inverse for Windows tiles
-    /*new FaviconsWebpackPlugin({
+    new FaviconsWebpackPlugin({
       logo: path.resolve(__dirname, 'src/public/img/logo.svg'),
       prefix: '/favicon/',
       favicons: {
@@ -79,7 +76,15 @@ module.exports = {
           yandex: false,
         },
       },
-    }),*/
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true,
+    }),
   ],
   output: {
     filename: 'script.min.js',
